5b9d7c10bf05e7065273860a333fe6328ea5e765
eustassBrian <bernalramirezb10@gmail.com>
2024-11-28 17:21:29 -0600

    add .css of acuse

--- js/acuseValidacion.js
+++ js/acuseValidacion.js
@@ -1,4 +1,14 @@
+// Función de validación de la contraseña
+function validarContrasena(contrasena) {
+    let regex = /^(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/; // Expresión regular
 
+    if (regex.test(contrasena)) {
+        console.log("Contraseña válida.");
+        alert("Inicio de sesión exitoso");
+    } else {
+        alert("La contraseña debe tener al menos 8 caracteres, incluir al menos un número y un carácter especial.");
+    }
+}
 
 // Función de validación del formulario
 function validarFormulario(event) {
@@ -13,8 +23,14 @@ function validarFormulario(event) {
 
         // Mostrar un mensaje de error
         alert("Por favor, ingrese los datos requeridos.");
-    }else {
-        alert("Sesion iniciada con exito");
+    } else {
+        // Validar la contraseña antes de enviar el formulario
+        if (validarContrasena(password)) {
+            // Si la contraseña es válida, proceder con el envío del formulario
+            alert("Formulario enviado.");
+        } else {
+            // Prevenir el envío del formulario si la contraseña no es válida
+            event.preventDefault();
+        }
     }
 }
-
